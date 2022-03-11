0ecce24c679cf797bbc01c4e947ef1c79f314505
Olufunke <olufunkeadejoke4@gmail.com>
2022-03-11 10:44:50 -0800

    just added the 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - Print text using printf
+ * Retuen: 0
+ **/
+int main(viod)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
