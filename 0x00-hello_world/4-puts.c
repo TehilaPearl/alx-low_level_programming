9e2c06f98cef43e95cb6ac9e0170ca7b83b06f88
Olufunke <olufunkeadejoke4@gmail.com>
2022-03-14 14:36:57 -0700

    just added a 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 /**
- * main - This fuction is the starting point
- * of the program that print a text to screen
- * Return: 0
- **/
+ * main - Entry point
+ * 
+ * Return: 0 (success)
+ */
 int main(viod)
 {
   puts("\"Programming is like building multilingual puzzle");
