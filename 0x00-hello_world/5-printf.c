e8278b4ca6838a126bfbaa3cc9432e7c850b3fc5
Olufunke <olufunkeadejoke4@gmail.com>
2022-03-14 16:00:56 -0700

    just added 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 /**
  * main - Print text using printf
- * Retuen: 0
- **/
+ *
+ * Return: Always 0 (Success)
+ */
 int main(viod)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
