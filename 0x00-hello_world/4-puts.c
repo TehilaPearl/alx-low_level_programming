74ff580701b4a87ca8bd9ba7b9cf5c966debc397
Olufunke <olufunkeadejoke4@gmail.com>
2022-03-14 16:06:57 -0700

    file 5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - prints the size of various types
  * Return: 0 if exited properly, non-zero otherwose
  */
-int main(viod)
+int main( viod)
 {
   printf("Size of a char: %d byte(s)\n", sizeof(char));
   printf("Size of an int: %d byte(s)\n", sizeof(int));
