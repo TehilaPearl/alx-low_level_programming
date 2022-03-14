752c0fb167a9e91ba9d0ff2b06046929050249f4
Olufunke <olufunkeadejoke4@gmail.com>
2022-03-14 15:07:46 -0700

    just added a 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: 0 (success)
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwose
  */
 int main(viod)
 {
-  puts("\"Programming is like building multilingual puzzle");
+  printf("Size of a char: %d byte(s)\n", sizeof(char));
+  printf("Size of an int: %d byte(s)\n", sizeof(int));
+  printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+  printf("Size of a long long int:%d byte(s)\n". size of (long long int));
+  printf("Size of a float: %d byte(s)\n", sizeof(float));
   return (0);
 }
