97f1b0e820f4c10fb63ad2bad848aac6b3b2cf90
Olufunke <olufunkeadejoke4@gmail.com>
2022-03-16 09:29:11 -0700

    added the 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,17 @@
 #include "main.h"
 /**
  * main - this is the starting point of the program
- * Return ; 0
+ * Return: 0
  **/
 int main(void)
 {
-  int i, size;
-  int str[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-  size = sizeof(str) / sizeof(int);
-  for (i = 0; i < size; i++)
-    {
-      _putchar(str[i]);
-      _putchar('\n');
-    }
-  return (0);
+int i, size;
+int str[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+size = sizeof(str) / sizeof(int);
+for (i = 0; i < size; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+return (0);
 }
