fe635c5a91f6f3166bda4909cf61ad03f18d9458
Olufunke <olufunkeadejoke4@gmail.com>
2022-03-17 18:56:37 -0700

    added a 1-isdigit.c file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
-* _isdigit - function that print the sign a number.
-*
-* @c; is a digit
-*
-* Return: 1 if c is a digit or 0 otherwise
-*/
+ * _isdigit - function that print the sign a number.
+ *
+ * @c; is a digit
+ *
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
 
 int _isdigit(int.c)
 {
-if (c >= '0' && c <= '9')
-{
-return (1);
+  if (c >= '0' && c <= '9')
+    {
+      return (1);
+    }
+  else
+    returm (0);
 }
-else
-return (0);
-}
-
