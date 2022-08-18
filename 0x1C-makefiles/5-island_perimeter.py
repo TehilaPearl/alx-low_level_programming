0be80fa2b5984fce111968963a1050cf9257c6ce
Olufunke <olufunkeadejoke4@gmail.com>
2022-08-17 17:33:06 -0700

    5 island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-""" module for island perimeter task """
+""" island_perimeter function module"""
 
 
 def island_perimeter(grid):
-    """ func to print perimeter of island """
-    water_zone = 0
-    land_zone = 1
-    cell_sidelen = 1
-    print("[], {}, {}".format(water_zone, land_zone, cell_sidelen))
-    
+    """ Function that returns the perimeter of island """
+
+    perimeter = 0
+
+    numberRows = len(grid)
+
+    if grid != []:
+        numberColumns = len(grid[0])
+
+    for a in range(numberRows):
+        for b in range(numberColumns):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == numberRows or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == numberColumns or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
